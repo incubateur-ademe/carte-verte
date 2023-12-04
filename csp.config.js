8e11caa13f8981c4c60227bea19e5ee0080cc05b
Lilian Saget-Lethias <lilian.sagetlethias@gmail.com>
2023-12-04 22:36:35 +0100

    chore: lower csp security

--- csp.config.js
+++ csp.config.js
@@ -3,7 +3,7 @@ connect-src 'self' https://*.gouv.fr;
 font-src 'self' data: blob:;
 media-src 'self' https://*.gouv.fr;
 img-src 'self' data: https://*.gouv.fr https://raw.githubusercontent.com/;
-script-src 'self' 'unsafe-inline' ${process.env.NODE_ENV === "development" ? "'unsafe-eval'" : ""};
+script-src 'self' 'https://*.gouv.fr' 'unsafe-inline' ${process.env.NODE_ENV === "development" ? "'unsafe-eval'" : ""};
 frame-src 'self' https://*.gouv.fr;
 style-src 'self' https://*.gouv.fr 'unsafe-inline';
 frame-ancestors 'self' https://*.gouv.fr;
@@ -11,12 +11,6 @@ object-src 'none';
 base-uri 'self' https://*.gouv.fr;
 form-action 'self' https://*.gouv.fr;
 block-all-mixed-content;
-upgrade-insecure-requests;
-${
-  process.env.NODE_ENV === "development"
-    ? ""
-    : `require-trusted-types-for 'script';
-        trusted-types react-dsfr react-dsfr-asap nextjs#bundler matomo-next;`
-}`.replace(/\n/g, " ");
+upgrade-insecure-requests;`.replace(/\n/g, " ");
 
 module.exports = ContentSecurityPolicy;
